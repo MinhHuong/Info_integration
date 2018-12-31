fbb8df39c684c6d043b78478028ac81abec9426c
Minh Huong Le Nguyen <minh-huong.le-nguyen@polytechnique.edu>
2018-12-31 16:54:04 +0100

    Merge inject_erroneous to master

--- code/inject_erroneous.py
+++ code/inject_erroneous.py
@@ -6,16 +6,17 @@ import random
 import io
 
 def inject(input_path,output_path,source,target): #source and target are 2 lists
-	'''
-	input : input_path: path to refalign
-			output_path: path to save refalign with injected errors
-			source: a list containing random subjects from source ontology (000)
-			target: a list containing random subjects from target ontology (001/ 002)
-	return : None		
+	"""
+	Inject erroneous sameAs statements.
+	We use minidom to parse refalign and to inject erroneous sameAs links.
 
-	We use minidom to parse refalign and to inject erroneous sameAs links
+	:param input_path: path to refalign
+	:param output_path: path to save refalign with injected errors
+	:param source: a list containing random subjects from source ontology (000)
+	:param target: a list containing random subjects from target ontology (001/ 002)
+	:return: None
+	"""
 
-	'''
 	assert (len(source) == len(target))
 	size = len(source)
 	doc = minidom.parse(input_path) # open existing file for parsing
@@ -49,25 +50,21 @@ def inject(input_path,output_path,source,target): #source and target are 2 lists
 	#doc.writexml(open(name+'.xml','w'))
 
 	doc = doc.toprettyxml()
+
 	# save file
 	text_file = open(output_path+".rdf", "w")
 	text_file.write(doc)
 	text_file.close()
 
 
-
-
-
 def correct_sameAs(path_refalign):
-	'''
-	input : path to refalign ex: '../data/001/refalign.rdf'
+	"""
+	This method parses the refalign file with minidom parser for XML.
 
-	This method parses the refalign file with minidom parser for XML
-
-	return : sameAs : A dictionary hat keeps correct SameAs Links between
+	:param path_refalign: ex '../data/001/refalign.rdf'
+	:return: A dictionary that keeps correct SameAs Links between
 	ontologies (source) and (target) defined in refalign
-
-	'''
+	"""
 	sameAs = {}
 	xmldoc = minidom.parse(path_refalign)
 	entity1_onto_source = xmldoc.getElementsByTagName('entity1')
@@ -84,15 +81,20 @@ def correct_sameAs(path_refalign):
 
 	return sameAs
 
+
 def random_subject(graph,dict_sameAs={},check_refalign=True,verbose=0):
-	'''
-	return: a list of randomly selected subjects 
+	"""
+
+	:param graph:
+	:param dict_sameAs:
+	:param check_refalign:
+	:param verbose:
+	:return: a list of randomly selected subjects
 	from an ontology (that we get as 'input')
-
-
-	'''
+	"""
 
 	random_subject = []
+
 	'''
 	graph.subjects() : subjects in an rdf triple - type: <class 'generator'>
 	We want to select 400 (no_erroneous) random subjects not existing as a key of 
@@ -100,6 +102,7 @@ def random_subject(graph,dict_sameAs={},check_refalign=True,verbose=0):
 	In order not to save all subjects in a list and then randomly select from them, 
 	we use sorted()
 	'''
+
 	for subject in sorted(graph.subjects(), key=lambda k: random.random()):
 		if verbose == 1:
 			print(str(subject))
@@ -117,13 +120,12 @@ def random_subject(graph,dict_sameAs={},check_refalign=True,verbose=0):
 	return random_subject
 
 
-
-
 if __name__ == '__main__':
 
-	no_erroneous = 400
-	verbose = 0
+	no_erroneous = 400 # number of erroneous sameAs statements to be injected
+	verbose = 0 # verbosity
 
+	# filenames
 	path_data_ref_001 = '../data/001/refalign.rdf'
 	path_data_ref_002 = '../data/002/refalign.rdf'
 	path_data_owl_000 = '../data/000/onto.owl'
@@ -133,19 +135,17 @@ if __name__ == '__main__':
 	sameAs_001 = correct_sameAs(path_data_ref_001) #Dict
 	sameAs_002 = correct_sameAs(path_data_ref_002) #Dict
 
-
 	func000 = PropertyExtraction(filename=path_data_owl_000)#graph
 	func001 = PropertyExtraction(filename=path_data_owl_001)#graph
 
-
-
+	# get the random URI's subject
 	random_source_001 = random_subject(func000.g,sameAs_001, check_refalign = True)
 	random_target_001 = random_subject(func001.g, check_refalign = False)
 
 	random_source_002 = random_subject(func000.g,sameAs_002, check_refalign = True)
 	random_target_002 = random_subject(func001.g, check_refalign = False)
 
-
+	# inject erroneous subjects in input graphs
 	inject(path_data_ref_001,'../data/001/refalign_err_injected',random_source_001,random_target_001)
 	inject(path_data_ref_002,'../data/002/refalign_err_injected',random_source_002,random_target_002)
 
