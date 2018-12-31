3c70eed9d8b465a8eaeb13aa9de7ef6dc7550a2a
armitakhn <32106810+armitakhn@users.noreply.github.com>
2018-12-31 06:31:21 -0800

    inject_erroneous.py injects incorrect SameAs links to refalign
    To inject incorrect sameAs links to refalign of ontology 001, 002

---
+++ Info_integration-Armita/code/inject_erroneous.py
@@ -0,0 +1,154 @@
+
+from pipeline import PropertyExtraction
+
+from xml.dom import minidom
+import random
+import io
+
+def inject(input_path,output_path,source,target): #source and target are 2 lists
+	'''
+	input : input_path: path to refalign
+			output_path: path to save refalign with injected errors
+			source: a list containing random subjects from source ontology (000)
+			target: a list containing random subjects from target ontology (001/ 002)
+	return : None		
+
+	We use minidom to parse refalign and to inject erroneous sameAs links
+
+	'''
+	assert (len(source) == len(target))
+	size = len(source)
+	doc = minidom.parse(input_path) # open existing file for parsing
+
+	alignment = doc.getElementsByTagName('Alignment')[0]
+	for i in range(size):
+		data = doc.createElement('map')
+		alignment.appendChild(data)
+		cell = doc.createElement('Cell')
+		data.appendChild(cell)
+
+		cons = doc.createElement('entity1')
+		cons.setAttribute('rdf:resource', source[i])
+		cell.appendChild(cons)
+
+		cons = doc.createElement('entity2')
+		cons.setAttribute('rdf:resource', target[i])
+		cell.appendChild(cons)
+
+		relation = doc.createElement('relation')
+		text = doc.createTextNode('=')
+		relation.appendChild(text)
+		cell.appendChild(relation)
+
+		measure = doc.createElement('measure')
+		measure.setAttribute('rdf:datatype', 'http://www.w3.org/2001/XMLSchema#float')
+		text = doc.createTextNode('1.0')
+		measure.appendChild(text)
+		cell.appendChild(measure)
+
+	#doc.writexml(open(name+'.xml','w'))
+
+	doc = doc.toprettyxml()
+	# save file
+	text_file = open(output_path+".rdf", "w")
+	text_file.write(doc)
+	text_file.close()
+
+
+
+
+
+def correct_sameAs(path_refalign):
+	'''
+	input : path to refalign ex: '../data/001/refalign.rdf'
+
+	This method parses the refalign file with minidom parser for XML
+
+	return : sameAs : A dictionary hat keeps correct SameAs Links between
+	ontologies (source) and (target) defined in refalign
+
+	'''
+	sameAs = {}
+	xmldoc = minidom.parse(path_refalign)
+	entity1_onto_source = xmldoc.getElementsByTagName('entity1')
+	entity2_onto_target = xmldoc.getElementsByTagName('entity2')
+
+	for i in range(len(entity1_onto_source)):
+		key = entity1_onto_source[i].attributes['rdf:resource'].value
+		value = entity2_onto_target[i].attributes['rdf:resource'].value
+		if key not in sameAs:
+			sameAs[key] = value
+		else:
+			raise ValueError('%s entity of source Ontology already\
+			 SameAs %s entity of target Ontology! ' %(key,value))
+
+	return sameAs
+
+def random_subject(graph,dict_sameAs={},check_refalign=True,verbose=0):
+	'''
+	return: a list of randomly selected subjects 
+	from an ontology (that we get as 'input')
+
+
+	'''
+
+	random_subject = []
+	'''
+	graph.subjects() : subjects in an rdf triple - type: <class 'generator'>
+	We want to select 400 (no_erroneous) random subjects not existing as a key of 
+	sameAs dictionary
+	In order not to save all subjects in a list and then randomly select from them, 
+	we use sorted()
+	'''
+	for subject in sorted(graph.subjects(), key=lambda k: random.random()):
+		if verbose == 1:
+			print(str(subject))
+			break
+		
+		if check_refalign:	
+			if subject not in dict_sameAs:
+				random_subject.append(str(subject))
+		else:
+			random_subject.append(str(subject))
+
+		if len(random_subject) == no_erroneous:
+			break
+
+	return random_subject
+
+
+
+
+if __name__ == '__main__':
+
+	no_erroneous = 400
+	verbose = 0
+
+	path_data_ref_001 = '../data/001/refalign.rdf'
+	path_data_ref_002 = '../data/002/refalign.rdf'
+	path_data_owl_000 = '../data/000/onto.owl'
+	path_data_owl_001 = '../data/001/onto.owl'
+	path_data_owl_002 = '../data/002/onto.owl'
+
+	sameAs_001 = correct_sameAs(path_data_ref_001) #Dict
+	sameAs_002 = correct_sameAs(path_data_ref_002) #Dict
+
+
+	func000 = PropertyExtraction(filename=path_data_owl_000)#graph
+	func001 = PropertyExtraction(filename=path_data_owl_001)#graph
+
+
+
+	random_source_001 = random_subject(func000.g,sameAs_001, check_refalign = True)
+	random_target_001 = random_subject(func001.g, check_refalign = False)
+
+	random_source_002 = random_subject(func000.g,sameAs_002, check_refalign = True)
+	random_target_002 = random_subject(func001.g, check_refalign = False)
+
+
+	inject(path_data_ref_001,'../data/001/refalign_err_injected',random_source_001,random_target_001)
+	inject(path_data_ref_002,'../data/002/refalign_err_injected',random_source_002,random_target_002)
+
+
+
+
---
+++ Info_integration-Armita/code/pipeline.py
@@ -0,0 +1,136 @@
+import rdflib as rdf
+import pprint as pp
+
+
+class PropertyExtraction():
+    """
+    This class is served as an utility to:
+    (1) compute the degree of functionality of a property from an RDF graph, and
+    (2) extract properties that are likely to be functional
+    """
+
+    def __init__(self, filename, verbose=0, threshold = 0.8):
+        """
+        Initialize stuffs
+        :param filename: filename to the input RDF graph
+        :param verbos: verbosity (?)
+        :param threshold: the threshold to decide if a property is functional
+        """
+        self.g = rdf.Graph().parse(source= filename , format='xml')
+        self.candidates = None
+        self.verbose = verbose
+        self.threshold = threshold
+        self.functional_properties = None
+
+    def build(self):
+        """
+        Build stuffs # TODO better documentation here...
+
+        :return: a set of functional properties
+        """
+        if self.verbose == 2:
+            print('building...!')
+
+        self.candidates = {}
+        self.functional_properties = set()
+
+        for s in self.g.subjects():
+            # (2.1) count the number of occurrences of each predicate p of s
+            predicates_occurrences = self.count_predicate_occurrences(self.g.predicate_objects(s))
+            if self.verbose == 1:
+                print(self.candidates)
+                break
+
+            # (2.2) for those predicates that appear only ONCE, update the functionality count
+            self.update_candidates(predicates_occurrences)
+
+        # (3) having the count of each predicate i.e. the number of times it associates one input to only one output
+        # compute the degree of functionality (a percentage?)
+        self.filter_functional_properties()
+
+        if self.verbose == 2:
+            print('functional_properties: ', self.functional_properties)
+
+        # return the set of functional properties
+        return self.functional_properties
+
+
+    def count_predicate_occurrences(self,predicates_objects):
+        """
+        Count the number of times a predicate p is associated to a given subject s e.g.
+        ex:a    rdf:type    ex:Person
+        ex:a    ex:work_at  ex:School
+        ex:a    ex:work_at  ex:Office
+        ex:a    ex:work_at  ex:France
+        Then the count should be: { "rdf:type" : 1, "ex:work_at": 3 }
+        Besides from counting the number of occurrences of predicates,
+        we should also update the total number of time it has appeared until now
+        i.e. to update the total count in candidates
+
+        :param predicates_objects: the list of tuples (predicate, object) associated to a subject
+        :return: a dictionary where k = predicate and v = its number of occurrences
+        """
+
+        # dictionary where k = predicate and v = the number of times p occurs (with a given subject)
+        occurrences = {}
+        for p_o in predicates_objects:
+            p = p_o[0]
+
+            # count the occurrences of a predicate p
+            if p not in occurrences:
+                occurrences[p] = 1
+            else:
+                occurrences[p] += 1
+
+            # update the total count of p
+            if p not in self.candidates:
+                # if p not yet added to candidates,
+                # initialize its total count to 1 and its functionality count to 0
+                self.candidates[p] = [0, 1]
+            else:
+                # else, just increment its total count
+                self.candidates[p][1] += 1
+
+        return occurrences
+
+
+    def update_candidates(self, occurrences):
+        """
+        Update the count of each candidate predicate.
+        :param candidates: the dictionary candidates
+        :param occurrences: the occurrences dictionary, where k = predicate p and v = number of occurrences of p
+        :return: None
+        """
+
+        # if a predicate appears only ONCE i.e. its number of occurrences = 1
+        # update it in the dictionary of candidates by incrementing its count (1st element) by 1
+        for p, count in occurrences.items():
+            if count == 1:
+                self.candidates[p][0] += 1 # update the functionality count
+
+
+    def filter_functional_properties(self):
+        """
+        Filter out properties that are unlikely to be functional,
+        by dividing functionality count by the total count of a predicate p.
+        We then compare the obtained ratio with the threshold,
+        if the ratio is >= the threshold, then p is possibly a functional property
+        :param candidates: the candidates dictionary
+        :param threshold: the functionality threshold
+        :return:
+        """
+        for p, counts in self.candidates.items():
+            deg = counts[0]/counts[1]
+            if deg >= self.threshold:
+                self.functional_properties.add(p)
+
+
+if __name__ == '__main__':
+
+    path_data_000 = '../data/000/onto.owl'
+    path_data_001 = '../data/001/onto.owl'
+
+    func000 = PropertyExtraction(filename=path_data_000, verbose=0, threshold=1.0)
+    func000.build()
+    print(func000.functional_properties)
+    print(len(func000.functional_properties))
